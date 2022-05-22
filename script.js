c590e057390075373a769109e20098435572074e
mikeebbot <102642240+mikeebbot@users.noreply.github.com>
2022-05-22 12:40:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function message () {
+    alert("Ninja was liked!");
+}
+function login (element) {
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+    }
+function hide (el) {
+    el.remove();
+}
